bf0e6af64d760cc5c778f0ddd6e8c337b2968d15
Mirek Kratochvil <exa.exa@gmail.com>
2020-03-08 17:43:37 +0100

    implement fast raster blending

--- src/GigaScatter.jl
+++ src/GigaScatter.jl
@@ -2,8 +2,12 @@ module GigaScatter
 
     import Images, FileIO, ImageMagick, Colors, Distributions
 
+    const Raster = Array{Float64, 3}
+    const RasterMix = Tuple{Array{Float64, 3}, Matrix{Float64}, Matrix{Float64}}
+
     include("colors.jl")
     include("rasterize.jl")
+    include("combine.jl")
     include("kernel.jl")
     include("export.jl")
 
@@ -20,6 +24,11 @@ module GigaScatter
 
     export #from kernel.jl
         rasterKernelCircle
+
+    export #from combine.jl.jl
+        mixableRaster,
+        mixRasters,
+        mixedRaster
 
     export #from export.jl
         solidBackground,
---
+++ src/combine.jl
@@ -0,0 +1,40 @@
+"""
+    mixableRaster(raster::Raster)::RasterMix
+
+Convert a raster into a form that is suitable for combining with other rasters.
+"""
+function mixableRaster(raster::Raster)::RasterMix
+    colors=copy(raster[1:3,:,:])
+    colors[1,:,:] .*= raster[4,:,:]
+    colors[2,:,:] .*= raster[4,:,:]
+    colors[3,:,:] .*= raster[4,:,:]
+    return (colors, 1 .- raster[4,:,:], raster[4,:,:])
+end
+
+"""
+    mixRasters(
+        (c1,ia1,w1,n1)::RasterMix,
+        (c2,ia2,w2,n2)::RasterMix)::RasterMix
+
+Combine 2 rasters into mixable form.
+"""
+function mixRasters(
+    (c1,ia1,w1)::RasterMix,
+    (c2,ia2,w2)::RasterMix)::RasterMix
+    return (c1 .+ c2, ia1 .* ia2, w1 .+ w2)
+end
+
+"""
+    mixedRaster((r,ia,w,n)::RasterMix)::Raster
+
+Convert several (possibly many) mixed rasters back into normal raster.
+"""
+function mixedRaster((r,ia,ws)::RasterMix)::Raster
+    w=copy(reshape(ws, (1,size(ws)...)))
+    w[w .== 0] .= 1
+    cat(dims=1,
+        r[1:1,:,:]./w,
+        r[2:2,:,:]./w,
+        r[3:3,:,:]./w,
+        1 .- reshape(ia,(1,size(ia)...)))
+end
